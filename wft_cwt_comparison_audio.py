16462148a5cd7b5b53667aabe62c778bfa317260
MelaTN <melanietn81@gmail.com>
2025-06-15 14:24:41 +0200

    Update wft_cwt_comparison_audio.py

--- wft_cwt_comparison_audio.py
+++ wft_cwt_comparison_audio.py
@@ -9,12 +9,12 @@ import os
 fs = 1000           # Sampling frequency (Hz)
 duration = 1.0      # Signal duration (seconds)
 window_type = 'hann' # WFT window shape
-window_size = 20000   # WFT window size
+window_size = 200   # WFT window size
 wavelet = "cmor1.5-1.0"
 scales = np.geomspace(1, 512, num=100)
 
 # Load the .wav file
-fs, data = wavfile.read(r'C:\Users\45298\Documents\GitHub\wavelet-cwt\samples\sangdrossel.wav')
+fs, data = wavfile.read(r'C:\Users\45298\Documents\GitHub\wavelet-cwt\samples\nattergal.wav')
 
 # Converting to mono
 if data.ndim > 1:
@@ -43,16 +43,25 @@ cwt_coeffs, cwt_freqs = pywt.cwt(data, scales, wavelet, 1.0 / fs)
 
 # --- PLOTTING --- #
 
+# First plot: the signal
+plt.figure(figsize=(10, 4))
+plt.plot(t, data)
+plt.title('Signal')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+# Second plot: WFT and CWT
 fig, axs = plt.subplots(2, 1, figsize=(9, 9), sharex=False)
 
-# Plot WFT
 im1 = axs[0].imshow(np.abs(wft_coeffs), aspect='auto',
                     extent=[wft_times[0], wft_times[-1], wft_freqs[0], wft_freqs[-1]],
                     origin='lower', cmap='viridis')
 axs[0].set_title("Windowed Fourier Transform (Spectrogram)")
 axs[0].set_ylim([0,10000])
 axs[0].set_ylabel("Frequency [Hz]")
-#fig.colorbar(im1, ax=axs[0], label='Magnitude')
 
 pcm = axs[1].pcolormesh(t, cwt_freqs, np.abs(cwt_coeffs))
 axs[1].set_ylim([0,10000])
